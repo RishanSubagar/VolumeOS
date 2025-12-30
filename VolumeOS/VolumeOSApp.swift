88d086319040cc47b5368ae4a138f0366fd053ba
RishanSubagar <rishans48@gmail.com>
2025-12-30 17:23:33 -0500

    Initial Commit

---
+++ VolumeOS/VolumeOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeOSApp.swift
+//  VolumeOS
+//
+//  Created by Rishan Subagar on 2025-12-30.
+//
+
+import SwiftUI
+
+@main
+struct VolumeOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
